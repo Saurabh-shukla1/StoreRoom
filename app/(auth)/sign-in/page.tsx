a219daa1105b4fe2388e3376dca051a66174a7a7
unknown <saurabhshukla6392@gmail.com>
2025-06-23 13:57:29 +0530

    Authentication with betterAuth enable email,google, github

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/sign-in-view";
+import { Sign } from "crypto";
+
+const SignInPage = () => {
+    console.log("SignInPage rendered");
+  return (
+    <SignInView />
+  );
+};
+
+export default SignInPage;
---
+++ modules/auth/ui/sign-in-view.tsx
@@ -0,0 +1,211 @@
+"use client";
+
+
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { OctagonAlertIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+;
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1, "Password is required"),
+});
+
+
+
+export const SignInView = () => {
+
+
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        setError(null);
+        setIsLoading(true);
+        authClient.signIn.email(
+            {
+                email: data.email,
+                password: data.password,
+                callbackURL: "/dashboard",
+            },
+            {
+                onSuccess: () => {
+                setIsLoading(false);
+            },
+            onError: ({error}) => {
+                setError(error.message);
+            }
+        })
+    }
+
+    const onSocial = (provider: "google" | "github") => {
+            setError(null);
+            setIsLoading(true);
+            authClient.signIn.social(
+                {
+                    provider: provider,
+                    callbackURL: "/dashboard",
+                },
+                {
+                    onSuccess: () => {
+                        setIsLoading(false);
+                    },
+                    onError: ({error}) => {
+                        setIsLoading(false);
+                        setError(error.message);
+                    }
+                }
+            )
+    }
+
+  return (
+    <div className="flex flex-col gap-6">
+        <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+                        <div className="flex flex-col gap-6">
+                            <div className="flex flex-col items-center text-center">
+                                <h1 className="text-2xl font-bold">
+                                    welcome back
+                                </h1>
+                                <p className="text-muted-foreground text-balance">
+                                    Login to your account
+                                </p>
+                            </div>
+                            <div className="grid gap-3">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input 
+                                                placeholder="you@example.com"
+                                                type="email" 
+                                                {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                </div>
+
+
+                            <div className="grid gap-3">
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input type="password" placeholder="••••••••" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            {!!error && (
+                                <Alert className="bg-destructive/10 border-none">
+                                    <OctagonAlertIcon className="h-4 w-4 !text-destructive"/>
+                                    <AlertTitle>{error}</AlertTitle>
+                                </Alert>
+                            )}
+
+                            <Button
+                                disabled={isLoading}
+                                className="w-full"
+                                type="submit"
+                            >
+                                Sign In
+                            </Button>
+                            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                    Or Continue with
+                                </span>
+                            </div>
+                            <div className="grid grid-cols-2 gap-4">
+                                <Button 
+                                disabled={isLoading}
+                                variant="outline" 
+                                className="w-full"
+                                type="button"
+                                onClick={() =>{
+                                    onSocial("google");
+                                }}
+                                >
+                                    <img src="/google.svg" alt="Google logo" className="h-5 w-5 mr-2" />
+                                    Google
+                                </Button>
+                                <Button 
+                                disabled={isLoading}
+                                variant="outline" 
+                                className="w-full"
+                                type="button"
+                                onClick={() =>{
+                                    onSocial("github");
+                                }}
+                                >
+                                    <img src="/github.svg" alt="GitHub logo" className="h-5 w-5 mr-2" />
+                                    GitHub
+                                </Button>
+                            </div>
+                            <div className="text-center text-sm">
+                                Don't have an account?{" "}
+                                <a href="/sign-up" className="text-primary underline">
+                                    Sign Up
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </Form>
+                <div className="bg-radial from-purple-800 to-pink-600 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <img src="/logo.svg" alt="logo" className="h-[92px] w-[92px]"/>
+                    <p className="text-2xl font-semibold text-white">
+                        Store-ROOM
+                    </p>
+                </div>
+            </CardContent>
+        </Card>
+        <div className="text-muted-foreground *:[a]:hover:tetx-primary text-center text-xs tyext-balance *:[a]underline *;[a]:underline-offset-4">
+           By signing in, you agree to our{" "}
+           <a href="/#" className="hover:text-primary">
+               Terms of Service
+           </a>{" "}
+           and{" "}
+           <a href="/#" className="hover:text-primary">
+               Privacy Policy
+           </a>.
+        </div>
+    </div>
+  );
+};
